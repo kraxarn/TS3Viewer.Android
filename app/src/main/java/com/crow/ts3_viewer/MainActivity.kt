84aa4f9036d092fea9598c78807c967c5374079b
kraxarn <me@kraxarn.com>
2019-07-28 20:55:40 +0200

    Cleaning up the mess

--- app/src/main/java/com/crow/ts3_viewer/MainActivity.kt
+++ app/src/main/java/com/crow/ts3_viewer/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -29,26 +30,42 @@ import com.google.android.material.textfield.TextInputLayout
 import org.slf4j.LoggerFactory
 import kotlin.concurrent.thread
 
+/**
+ * Toggles the view depending on if there are any items
+ */
+class ServerListObserver(private val view: View) : RecyclerView.AdapterDataObserver()
+{
+    override fun onItemRangeInserted(positionStart: Int, itemCount: Int)
+    {
+        update(itemCount)
+        super.onItemRangeInserted(positionStart, itemCount)
+    }
+
+    fun update(itemCount: Int)
+    {
+        view.visibility = if (itemCount == 0) View.VISIBLE else View.GONE
+    }
+}
+
 class MainActivity : AppCompatActivity()
 {
     /**
-     * Text inputs used in the "add" dialog
+     * Temporary re-usable TS3 instance
      */
-    private lateinit var textInputs: Array<TextInputLayout>
-
-    private var addDialog: AlertDialog? = null
-
-    private lateinit var addLoad: LinearLayout
-
     private var ts3: Ts3? = null
 
+    /**
+     * List of server for the recycler view
+     */
     private val serverList = mutableListOf<ServersEntry>()
 
+    /**
+     * Server list adapter
+     */
     private val serverListAdapter = ServersAdapter(serverList)
 
-    private lateinit var textNoServers: TextView
-
-    // Progress stuffs
+    //region Server info
+    // This is here since we reuse the same stuff
 
     private lateinit var serverInfoLoad: ProgressBar
 
@@ -56,20 +73,17 @@ class MainActivity : AppCompatActivity()
 
     private lateinit var serverInfoList: RecyclerView
 
+    //endregion
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
+        // Create and inflate etc.
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set the action bar title
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.title = "${getString(R.string.app_name)} - Welcome"
-
-        val addDialogView = layoutInflater.inflate(R.layout.dialog_add, findViewById(R.id.parent), false)
-
-        textInputs = arrayOf(
-            addDialogView.findViewById(R.id.text_layout_add_ip),
-            addDialogView.findViewById(R.id.text_layout_add_query)
-        )
-        addLoad = addDialogView.findViewById(R.id.layout_add_load)
 
         // Setup server info sheet
         val serverInfoView = findViewById<LinearLayout>(R.id.layout_server_info)
@@ -77,16 +91,20 @@ class MainActivity : AppCompatActivity()
         serverInfo.state = BottomSheetBehavior.STATE_HIDDEN
         serverInfoLoad = findViewById(R.id.progress_info_load)
 
-        // Check if no servers added yet
-        textNoServers = findViewById(R.id.text_no_servers)
-        if (serverList.isEmpty())
-            textNoServers.visibility = View.VISIBLE
+        // TODO: Load from file here
 
+        // Testing stuffs
+        serverListAdapter.registerAdapterDataObserver(ServerListObserver(findViewById(R.id.text_no_servers)).apply {
+            this.update(serverList.size)
+        })
+
+        // Setup server list
         findViewById<RecyclerView>(R.id.list_servers).apply {
             adapter       = serverListAdapter
             layoutManager = LinearLayoutManager(this@MainActivity)
         }
 
+        // Setup the list for channels/users in server info
         serverInfoList = findViewById(R.id.view_server_info)
         LinearLayoutManager(this).let {
             serverInfoList.layoutManager = it
@@ -95,13 +113,15 @@ class MainActivity : AppCompatActivity()
 
         // Scary stuff
         findViewById<FloatingActionButton>(R.id.fab_add).setOnClickListener {
-            // If we already dismissed it
-            if (addDialog != null)
-                (addDialogView.parent as ViewGroup).removeAllViews()
-            // Just to make sure it's all good
-            setTextInputsEnabled(true)
+            // Inflate view
+            val addDialogView = layoutInflater.inflate(R.layout.dialog_add, findViewById(R.id.parent), false)
+            val textInputs = arrayOf<TextInputLayout>(
+                addDialogView.findViewById(R.id.text_layout_add_ip),
+                addDialogView.findViewById(R.id.text_layout_add_query)
+            )
+            val addLoad = addDialogView.findViewById<LinearLayout>(R.id.layout_add_load)
             // Create and show dialog
-            addDialog = MaterialAlertDialogBuilder(this).apply {
+            MaterialAlertDialogBuilder(this).apply {
                 setTitle("Add New Server")
                 setTheme(R.style.AppTheme_Dialog)
                 setView(addDialogView)
@@ -109,39 +129,43 @@ class MainActivity : AppCompatActivity()
                 setNegativeButton("Cancel", null)
             }.create().apply {
                 setOnShowListener {
-                    val dialog = addDialog as AlertDialog
-                    dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {view ->
+                    getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener { view ->
                         // Pressed "add"
-                        val errorText = dialog.findViewById<TextView>(R.id.text_add_error)
+                        // Save error text for now and hide it
+                        val errorText = findViewById<TextView>(R.id.text_add_error)
                         errorText?.visibility = View.GONE
-                        setTextInputsEnabled(false)
+                        // Disable text entries while loading
+                        setTextInputsEnabled(textInputs, addLoad, false)
                         view.isEnabled = false
                         // Create TS3 API instance
                         ts3 = Ts3(
-                            dialog.findViewById<TextInputEditText>(R.id.edit_text_add_ip)
-                                ?.text.toString()
+                            findViewById<TextInputEditText>(R.id.edit_text_add_ip)?.text.toString()
                         )
                         // Try to connect (on another thread)
                         thread(true)
                         {
                             if (ts3!!.connect())
                             {
+                                // If successful, add to server list
                                 serverList.add(ts3!!.toEntry(this@MainActivity))
                                 this@MainActivity.runOnUiThread {
-                                    textNoServers.visibility = View.GONE
-                                    serverListAdapter.notifyDataSetChanged()
-                                    dialog.dismiss()
+                                    serverListAdapter.notifyItemInserted(serverList.size - 1)
+                                    dismiss()
                                 }
                                 // For now, this is all we get
                                 ts3?.exit()
                                 ts3 = null
+                                // Save new server to file first
+                                Config(this@MainActivity).save(serverList.map {
+                                    it.data
+                                })
                             }
                             else
                             {
-                                // Failed to connect
+                                // Failed to connect, show error and enable text entries
                                 view.post {
                                     errorText?.visibility = View.VISIBLE
-                                    setTextInputsEnabled(true)
+                                    setTextInputsEnabled(textInputs, addLoad, true)
                                     view.isEnabled = true
                                 }
                             }
@@ -153,12 +177,18 @@ class MainActivity : AppCompatActivity()
         }
     }
 
+    /**
+     * Loads the menu from the action bar
+     */
     override fun onCreateOptionsMenu(menu: Menu?): Boolean
     {
         menuInflater.inflate(R.menu.menu_app_options, menu)
         return true
     }
 
+    /**
+     * Button in the action bar menu was pressed
+     */
     override fun onOptionsItemSelected(item: MenuItem): Boolean
     {
         return when (item.itemId)
@@ -179,7 +209,7 @@ class MainActivity : AppCompatActivity()
         }
     }
 
-    private fun setTextInputsEnabled(enabled: Boolean)
+    private fun setTextInputsEnabled(textInputs: Array<TextInputLayout>, addLoad: View, enabled: Boolean)
     {
         for (input in textInputs)
             input.isEnabled = enabled
@@ -187,6 +217,9 @@ class MainActivity : AppCompatActivity()
         addLoad.visibility = if (enabled) View.GONE else View.VISIBLE
     }
 
+    /**
+     * Opens the server info for the specified TS3 instance
+     */
     fun showServerInfo(ts3: Ts3)
     {
         // Start showing loading
